a00a92d720bd63c989ec314ccbf237fa92f19731
r2d2 <nclsmarc@gmail.com>
2012-04-04 18:10:06 +0200

    Added support of gimli branch (xbmc with vmeta accel) Possibility to choose branch Corrected marvellgfx package

--- package/cuboxxbmc/cuboxxbmc.mk
+++ package/cuboxxbmc/cuboxxbmc.mk
@@ -3,13 +3,17 @@
 # cuboxxbmc
 #
 #############################################################
-CUBOXXBMC_VERSION = HEAD
+
+# This is to compile gimli branch : marvell-dove branch
+ifeq ($(BR2_PACKAGE_CUBOXXBMC_GIMLI),y)
+
+CUBOXXBMC_VERSION = 45b2727e792cc0ac07cbe0212aeb717a0866bd45
 CUBOXXBMC_SOURCE = cuboxxbmc-$(CUBOXXBMC_VERSION).tar.gz
-CUBOXXBMC_SITE = git://github.com/rabeeh/xbmc.git
+CUBOXXBMC_SITE = git://github.com/huceke/xbmc.git
 CUBOXXBMC_INSTALL_STAGING = YES
 CUBOXXBMC_INSTALL_TARGET = YES
-CUBOXXBMC_CONF_OPT = --enable-gles --enable-dove --enable-dove-overlay --enable-gstreamer --disable-pulse --disable-vdpau
-CUBOXXBMC_DEPENDENCIES = boost sqlite mysql_client libass libmpeg2 gstreamer libmad jpeg tiff libsamplerate libogg libvorbis libmodplug libcurl flac lzo zlib bzip2 gst-plugins-good yajl libgcrypt pcre libcdio libjasper dbus sdl sdl_mixer sdl_image libmicrohttpd samba python gperf marvellgfx xlib_libXdamage xlib_libXfixes xlib_libXrandr libgcrypt openssl xlib_libXext afpfsng libnfs libplist libshairport librtmp libusb avahi
+CUBOXXBMC_CONF_OPT = --enable-gles --disable-pulse --disable-projectm --enable-dbus --disable-optimizations --enable-debug --with-platform=marvell-dove
+CUBOXXBMC_DEPENDENCIES = boost sqlite mysql_client libass libmpeg2 libmad jpeg tiff libsamplerate libogg libvorbis libmodplug libcurl flac lzo zlib bzip2 gst-plugins-good yajl libgcrypt pcre libcdio libjasper dbus sdl sdl_mixer sdl_image libmicrohttpd samba python gperf marvellgfx xlib_libXdamage xlib_libXfixes xlib_libXrandr libgcrypt openssl xlib_libXext afpfsng libnfs libplist libshairport librtmp libusb avahi fribidi
 
 define CUBOXXBMC_BOOTSTRAP
 	if [ ! -e $(CUBOXXBMC_DIR)/configure ] ; then cd $(CUBOXXBMC_DIR) && ./bootstrap ; fi
@@ -18,6 +22,29 @@ endef
 define CUBOXXBMC_ENV
 	CPPFLAGS="-m32"
 endef
+
+endif
+
+# This is to compile from Rabeeh branch
+ifeq ($(BR2_PACKAGE_CUBOXXBMC_RABEEH),y)
+
+CUBOXXBMC_VERSION = HEAD
+CUBOXXBMC_SOURCE = cuboxxbmc-$(CUBOXXBMC_VERSION).tar.gz
+CUBOXXBMC_SITE = git://github.com/rabeeh/xbmc.git
+CUBOXXBMC_INSTALL_STAGING = YES
+CUBOXXBMC_INSTALL_TARGET = YES
+CUBOXXBMC_CONF_OPT = --enable-gles --enable-dove --enable-dove-overlay --enable-gstreamer --disable-pulse --disable-vdpau
+CUBOXXBMC_DEPENDENCIES = boost sqlite mysql_client libass libmpeg2 gstreamer libmad jpeg tiff libsamplerate libogg libvorbis libmodplug libcurl flac lzo zlib bzip2 gst-plugins-good yajl libgcrypt pcre libcdio libjasper dbus sdl sdl_mixer sdl_image libmicrohttpd samba python gperf marvellgfx xlib_libXdamage xlib_libXfixes xlib_libXrandr libgcrypt openssl xlib_libXext afpfsng libnfs libplist libshairport librtmp libusb avahi fribidi
+
+define CUBOXXBMC_BOOTSTRAP
+	if [ ! -e $(CUBOXXBMC_DIR)/configure ] ; then cd $(CUBOXXBMC_DIR) && ./bootstrap ; fi
+endef
+
+define CUBOXXBMC_ENV
+	CPPFLAGS="-m32"
+endef
+
+endif
 
 CUBOXXBMX_CONF_ENV += CUBOXXBMC_ENV
 CUBOXXBMC_PRE_CONFIGURE_HOOKS += CUBOXXBMC_BOOTSTRAP
--- package/marvellgfx/marvellgfx.mk
+++ package/marvellgfx/marvellgfx.mk
@@ -4,28 +4,33 @@
 #
 #############################################################
 MARVELLGFX_VERSION = 1.0
-MARVELLGFX_SOURCE = libgfx.tar.gz
+MARVELLGFX_SOURCE = packages.tar.gz
 MARVELLGFX_SITE = http://dl.dropbox.com/u/9548900/debian_packages
 MARVELLGFX_INSTALL_STAGING = YES
 MARVELLGFX_INSTALL_TARGET = YES
 MARVELLGFX_DEPENDENCIES = 
 
 define MARVELLGFX_BUILD_CMDS
-	cd $(@D) && tar xzvf marvell-libgfx_0.2.0-0ubuntu1~ppa15.tar.gz
+	cd $(@D)/marvell-ipp && tar xzvf marvell-ipp_0.2.1-0ubuntu1~ppa10.tar.gz
 endef
 
 define MARVELLGFX_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libgfx/bin/*.so* $(STAGING_DIR)/usr/lib/
-	cp -rf $(@D)/libgfx/include/* $(STAGING_DIR)/usr/include/
+	$(INSTALL) -d $(STAGING_DIR)/usr/include/marvell-ipp
+	$(INSTALL) -D -m 0755 $(@D)/libgfx/libgfx/bin/*.so* $(STAGING_DIR)/usr/lib/
+	cp -rf $(@D)/libgfx/libgfx/include/* $(STAGING_DIR)/usr/include/
+	$(INSTALL) -D -m 0755 $(@D)/marvell-ipp/marvell-ipp/bin/*.so* $(STAGING_DIR)/usr/lib/
+	cp -rf $(@D)/marvell-ipp/marvell-ipp/include/* $(STAGING_DIR)/usr/include/marvell-ipp
 endef
 
 define MARVELLGFX_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/lib/modules
 	$(INSTALL) -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -d $(TARGET_DIR)/etc
-	$(INSTALL) -D -m 0755 $(@D)/libgfx/bin/*.ko $(TARGET_DIR)/lib/modules/
-	$(INSTALL) -D -m 0755 $(@D)/libgfx/bin/*.so* $(TARGET_DIR)/usr/lib/
-	cp -rf $(@D)/libgfx/etc/* $(TARGET_DIR)/etc/
+#	$(INSTALL) -D -m 0755 $(@D)/libgfx/libgfx/bin/*.ko $(TARGET_DIR)/lib/modules/
+#	cp -rf $(@D)/packages/libgfx/libgfx/etc/* $(TARGET_DIR)/etc/
+	$(INSTALL) -D -m 0755 $(@D)/libgfx/libgfx/bin/*.so* $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -D -m 0755 $(@D)/marvell-ipp/marvell-ipp/bin/*.so* $(TARGET_DIR)/usr/lib/
+	cp -rf $(@D)/marvell-ipp/marvell-ipp/etc/* $(TARGET_DIR)/etc/
 endef
 
 $(eval $(call GENTARGETS,package,marvellgfx))
