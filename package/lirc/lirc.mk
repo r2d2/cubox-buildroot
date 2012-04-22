d34bbf1fe6742b89600f9da858c8d8e4c91b7e5e
r2d2 <nclsmarc@gmail.com>
2012-04-22 20:08:30 +0200

    Adding avahi daemon for airplay support Using lirc userspace driver

--- package/lirc/lirc.mk
+++ package/lirc/lirc.mk
@@ -9,7 +9,7 @@ LIRC_SOURCE = lirc-$(LIRC_VERSION).tar.gz
 LIRC_SITE = http://prdownloads.sourceforge.net/lirc
 LIRC_INSTALL_STAGING = YES
 LIRC_INSTALL_TARGET = YES
-LIRC_CONF_OPT = --without-x --with-driver=serial --enable-sandboxed
+LIRC_CONF_OPT = --without-x --with-driver=userspace --enable-sandboxed
 LIRC_DEPENDENCIES = 
 LIRC_AUTORECONF = YES
 
